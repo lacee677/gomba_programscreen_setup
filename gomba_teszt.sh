ac073498ba0159796cb624e37215d19d362025bf
László Misurák <lacee677@gmail.com>
2018-04-24 22:47:28 +0200

    commit 6

--- gomba_teszt.sh
+++ gomba_teszt.sh
@@ -14,7 +14,7 @@ setxkbmap -option terminate:ctrl_alt_bksp
 # Start Chromium in kiosk mode
 sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/'Local State'
 sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences
-chromium-browser --disable-cpu --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --disable translate --kiosk --kiosk 'https://gombaszog.github.io/programscreen'" | sudo tee /etc/xdg/openbox/autostart > /dev/null
+chromium-browser --disable-cpu --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --disable-translate --kiosk 'https://gombaszog.github.io/programscreen'" | sudo tee /etc/xdg/openbox/autostart > /dev/null
 echo "config added to /etc/xdg/openbox/autostart (for chrome autostart)"
 
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor' | sudo tee /home/pi/.bash_profile > /dev/null
@@ -51,27 +51,4 @@ fi
 
 echo "script finished. You can restart the PI now (reboot)"
 
-#Old config
-#[Desktop Entry]
-#Encoding=UTF-8
-#Type=Application
-#Exec=/usr/bin/chromium-browser --disable-cpu --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --disable translate --kiosk https://gombaszog.github.io/programscreen
-#Hidden=false
-#X-GNOME-Autostart-enabled=true
-#Name[en_US]=AutoChromium
-#Name=AutoChromium
-
-
 #https://die-antwort.eu/techblog/2017-12-setup-raspberry-pi-for-kiosk-mode/
-# Disable any form of screen saver / screen blanking / power management
-#xset s off
-#xset s noblank
-#xset -dpms
-
-# Allow quitting the X server with CTRL-ATL-Backspace
-#setxkbmap -option terminate:ctrl_alt_bksp
-
-# Start Chromium in kiosk mode
-#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-#chromium-browser --disable-infobars --kiosk 'https://gombaszog.github.io/programscreen'
